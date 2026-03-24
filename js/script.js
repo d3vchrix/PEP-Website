8c178b4da0f4bf52a739cf57e4b91c347e76bbf7
chris <dev.chrispadullo@outlook.com>
2026-03-24 21:58:11 +0800

    add new pages

--- js/script.js
+++ js/script.js
@@ -3,14 +3,28 @@ const leftArrow = document.getElementById('left-arrow');
 const rightArrow = document.getElementById('right-arrow');
 
 leftArrow.addEventListener('click', function () {
-    // Get the exact width of one project item
     const itemWidth = document.querySelector('.project-item').offsetWidth;
-    // Scroll left by that width
     projectContainer.scrollBy({ left: -itemWidth, behavior: 'smooth' });
 });
 
 rightArrow.addEventListener('click', function () {
     const itemWidth = document.querySelector('.project-item').offsetWidth;
-    // Scroll right by that width
     projectContainer.scrollBy({ left: itemWidth, behavior: 'smooth' });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollTopBtn = document.getElementById("scrollTopBtn");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 300) {
+            scrollTopBtn.classList.add("show");
+        } else {
+            scrollTopBtn.classList.remove("show");
+        }
+    });
+    scrollTopBtn.addEventListener("click", function() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
