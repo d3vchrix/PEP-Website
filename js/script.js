5f4f8d9de60ce177f634a09ee40f8e5dade791b7
chris <dev.chrispadullo@outlook.com>
2026-03-25 19:40:05 +0800

    Fix: index.html, ourwork-marketing.html, css/base.css, js/script.js

--- js/script.js
+++ js/script.js
@@ -29,3 +29,26 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+
+const element = document.getElementById('text-target');
+  const words = element.innerText.split(' '); 
+  const limit = 10; 
+
+  if (words.length > limit) {
+    element.innerText = words.slice(0, limit).join(' ') + '...';
+  }
+  const element2 = document.getElementById('text-target2');
+  const words2 = element2.innerText.split(' '); 
+  const limit2 = 10; 
+
+  if (words2.length > limit2) {
+    element2.innerText = words2.slice(0, limit2).join(' ') + '...';
+  }
+  
+  const element3 = document.getElementById('text-target3');
+  const words3 = element3.innerText.split(' '); 
+  const limit3 = 10; 
+
+  if (words3.length > limit3) {
+    element3.innerText = words3.slice(0, limit3).join(' ') + '...';
+  }
